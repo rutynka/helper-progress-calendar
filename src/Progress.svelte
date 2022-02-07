b97a72cb4232f4d3f68454b87b99490a07465ebe
papamd <rutynka@protonmail.com>
2022-02-07 18:54:47 +0100

    Add svelte-jest workflow

--- src/Progress.svelte
+++ src/Progress.svelte
@@ -1,29 +1,32 @@
 <svelte:options accessors={true} />
 
 <script>
-	export let LANG = 'en'
-	export let DAYS = 14
+	export let lang = 'en'
 	export let show = false
-	export let DAYS_BEFORE = 13
+	export let show_time = true
+	export let visible_days = 14
+	export let days_before_today = 13
 	export let localstorage_key = window.location.href
 
 	let today = new Date()
 	let css_today = search_date_format(today)
-	today.setDate(today.getDate() - DAYS_BEFORE)
+	today.setDate(today.getDate() - days_before_today)
 	let calendar = init_calendar_day_sequence()
 
 	export const store_progress = function(ev) {
-		ev = ev || {}
+		ev = ev ?? {}
 		set_default(ev)
 		if (ev.store_db) {
 			send_progress(ev)
 		}
 		add_to_local_storage(ev)
-		update_calendar(packHitsInaDay(get_storage()))
+		build_calendar_with_visible_days(pack_hits_in_a_day(get_storage()))
+		match_days_with_time(get_storage())
 		show = true;
 	};
 
-	console.log('progress helper loaded v0.0.5')
+	console.log('progress helper loaded v0.0.9')
+	// [{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-03"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-04"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-04"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-05"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-06"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-06"},{"collectionName":"Samochody-Maserati-binary","dT":"2022-2-06"}]
 
 	function set_default(ev) {
 		localstorage_key = ev.collectionName ? ev.collectionName : window.location.href
@@ -60,11 +63,12 @@
 	function init_calendar_day_sequence() {
 		let sequence = []
 		let today = new Date()
-		today.setDate(today.getDate() - DAYS_BEFORE)
-		for (let i = 0; i<DAYS; i++) {
+		today.setDate(today.getDate() - days_before_today)
+		for (let i = 0; i<visible_days; i++) {
 
 			sequence.push({
 				val: 0,
+				sec: 0,
 				title: date_format(new Date(today.setDate(today.getDate() + 1))),
 				search_date: search_date_format(new Date(today.setDate(today.getDate())))
 			})
@@ -73,20 +77,20 @@
 	}
 
 	function date_format(d) {
-		const ye = new Intl.DateTimeFormat(LANG, { year: 'numeric' }).format(d);
-		const mo = new Intl.DateTimeFormat(LANG, { month: 'short' }).format(d);
-		const da = new Intl.DateTimeFormat(LANG, { day: '2-digit' }).format(d);
+		const ye = new Intl.DateTimeFormat(lang, { year: 'numeric' }).format(d);
+		const mo = new Intl.DateTimeFormat(lang, { month: 'short' }).format(d);
+		const da = new Intl.DateTimeFormat(lang, { day: '2-digit' }).format(d);
 		return `${da}-${mo}-${ye}`
 	}
 
 	function search_date_format(d) {
-		const ye = new Intl.DateTimeFormat(LANG, { year: 'numeric' }).format(d);
-		const mo = new Intl.DateTimeFormat(LANG, { month: 'numeric' }).format(d);
-		const da = new Intl.DateTimeFormat(LANG, { day: '2-digit' }).format(d);
+		const ye = new Intl.DateTimeFormat(lang, { year: 'numeric' }).format(d);
+		const mo = new Intl.DateTimeFormat(lang, { month: 'numeric' }).format(d);
+		const da = new Intl.DateTimeFormat(lang, { day: '2-digit' }).format(d);
 		return `${ye}-${mo}-${da}`
 	}
 
-	function packHitsInaDay(eventsData) {
+	function pack_hits_in_a_day(eventsData) {
 		let eventByDates = {}
 		if (eventsData.length <= 0) {
 			console.log('event data not found or invalid localstorage key')
@@ -109,7 +113,17 @@
 		return eventByDates
 	}
 
-	function update_calendar(eventsByDate) {
+	function match_days_with_time(eventsData) {
+		let eventByTime = {}
+		if (eventsData.length <= 0) {
+			console.log('no events data')
+			return {}
+		}
+		console.log(eventsData)
+		return eventByTime
+	}
+
+	function build_calendar_with_visible_days(eventsByDate) {
 		let dateEventsKey = Object.keys(eventsByDate)
 		for (let i = 0; i< calendar.length; i++) {
 			if (dateEventsKey.indexOf(calendar[i].search_date) !== -1) {
@@ -135,30 +149,42 @@
 </script>
 
 <prgs-calendar class="prgs">
-		{#if show}
-			<div id="cal" class="days">
-				{#each calendar as row }
-				<div data-cal={row.search_date} title="{row.title}" class="{row.search_date === css_today ? 'today ' : ''}sq big sqc-{row.val}"></div>
-				{/each}
+	{#if show}
+		<div id="cal" class="days">
+			{#each calendar as row }
+			<div data-cal={row.search_date} title="{row.title}" class="{row.search_date === css_today ? 'today ' : ''}sq big sqc-{row.val}">
+			{#if row.sec && show_time }
+				<span class="sec">{row.sec}</span>
+			{/if}
 			</div>
-			<div class="legend prgs">
-				<span>Less</span>
-				<div class="sq tiny sqc-0"></div>
-				<div class="sq tiny sqc-1"></div>
-				<div class="sq tiny sqc-2"></div>
-				<div class="sq tiny sqc-3"></div>
-				<div class="sq tiny sqc-4"></div>
-				<div class="sq tiny sqc-5"></div>
-				<div class="sq tiny sqc-6"></div>
-				<span>More</span>
-			</div>
-		{/if}
+			{/each}
+		</div>
+		<div class="legend prgs">
+			<span>Less</span>
+			<div class="sq tiny sqc-0"></div>
+			<div class="sq tiny sqc-1"></div>
+			<div class="sq tiny sqc-2"></div>
+			<div class="sq tiny sqc-3"></div>
+			<div class="sq tiny sqc-4"></div>
+			<div class="sq tiny sqc-5"></div>
+			<div class="sq tiny sqc-6"></div>
+			<span>More</span>
+		</div>
+	{/if}
 </prgs-calendar>
 
 <style>
 	:root {
 		--green-hue: 131;
-		--blue-hue: 153;
+		--blue-hue: 300;
+	}
+	span.sec {
+		top:15px;
+		display: block;
+		text-align: center;
+		font-size: 0.5rem;
+		font-family: monospace;
+		color:black;
 	}
 	span {
 		position: relative;
@@ -179,11 +205,12 @@
 		padding:15px;
 	}
 	.prgs.legend {
-		bottom: 13px;
+		top: -24px;
 		display: flex;
 		justify-content: flex-end;
 		color:white;
-		padding:5px 0;
+		background-color: transparent;
+		position: relative;
 	}
 	.sq.sqc-0 {
 		background-color: hsla(var(--green-hue), 5%, 74%,1);
@@ -221,7 +248,8 @@
 		border-radius: 3px;
 	}
 	.today.sq.big {
-		border:2px solid hsla(var(--blue-hue), 75%, 74%,1);
+		border:2px solid hsla(var(--blue-hue), 100%, 30%,1);
+		border-radius: 16px;
 	}
 	.sq.tiny {
 		height: 10px;
@@ -235,6 +263,11 @@
 			margin: 2px 2px;
 			height: 15px;
 			width: 15px;
+		}
+		span.sec {
+			font-size: 0.4rem;
+			right: 3px;
+			top:8px;
 		}
 	}
 </style>
